7c9e09feddd90e6e6734fd6fc352a5d237888d60
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-03-22 11:53:13 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+//******************---------MODAL POPUP FUNCTION-------**************************// -->
+  const popup = document.querySelector('.popup');
+  window.onload = function (){
+    setTimeout(function(){
+      popup.classList.add("open-popup")
+    }, 50)
+  }
+  // <!-- //******************---------MODAL POPUP FUNCTION-------**************************// -->
+
+
+  // <!-- //******************---------MODAL POPUP CLOSE FUNCTION-------**************************// -->
+  function closePopup(){
+    var img = document.getElementById('popImg');
+    img.src = 'otacon1.png';
+    document.getElementById('btn').style.color = "rgb(197, 144, 197)";
+    document.getElementById('btn').style.background = "rgb(86, 31, 89)";
+    setTimeout(() => {
+      closeclose();
+    }, 2000);
+  }
+
+
+
+  function closeclose(){
+    popup.classList.remove("open-popup")
+    document.getElementById("headerContainer").style.visibility = "visible"
+    document.getElementById("about").style.visibility = "visible"
+    document.getElementById("about").style.display = "flex"
+    }
+  
+    // <!-- //******************---------MODAL POPUP CLOSE FUNCTION-------**************************// -->
+
+
+    // <!-- //******************---------MODAL RESPONSIVE NAV-------**************************// -->
+
+function dropdown(){
+ var dropdown = document.getElementById('myDropdown')
+dropdown.classList.toggle(".dropdown-show")
+
+}
+
+
+
+// <!-- //******************---------MODAL RESPONSIVE NAV-------**************************// -->
+
