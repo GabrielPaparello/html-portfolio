70667d3a88a39f17e40f92871fc4f24d904d7083
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-03-22 14:26:44 -0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+//******************---------MODAL POPUP FUNCTION-------**************************// -->
+  
+
+
+
+
 
 
 
 
 
 //******************---------MODAL POPUP FUNCTION-------**************************// -->
-  const popup = document.getElementById('popup');
-  window.onload = function (){
+//******************---------MODAL POPUP FUNCTION-------**************************// -->
+  const popup = $('#popup');
+  // window.onload = 
+  function (){
     setTimeout(function(){
-      popup.classList.add("open-popup")
+      popup.addClass("open-popup");
     }, 50)
   }
   // <!-- //******************---------MODAL POPUP FUNCTION-------**************************// -->
@@ -15,22 +23,25 @@
 
   // <!-- //******************---------MODAL POPUP CLOSE FUNCTION-------**************************// -->
   function closePopup(){
-    var img = document.getElementById('popImg');
+    var img = $('#popImg');
+    var btnj = $('#btn');
     img.src = 'otacon1.png';
-    document.getElementById('btn').style.color = "rgb(197, 144, 197)";
-    document.getElementById('btn').style.background = "rgb(86, 31, 89)";
-    setTimeout(() => {
-      closeclose();
-    }, 2000);
-  }
+    btnj.click(function(){
+
+      $('#popImg').attr("src", "otacon1.png");
+      $('#btn').css({'color' : 'rgb(197, 144, 197', 'background-color': "rgb(86, 31, 89)"});      
+      setTimeout(() => {
+        closeclose();
+      }, 2000);
+  })}
 
 
 
   function closeclose(){
-    popup.classList.remove("open-popup")
-    document.getElementById("headerContainer").style.visibility = "visible"
-    document.getElementById("about").style.visibility = "visible"
-    document.getElementById("about").style.display = "flex"
+    popup.classAdd("open-popup")
+    $("#headerContainer").css(display, visible);
+    $("#about").css(display, visible);
+    $("#about").css(display, flex);
     }
   
     // <!-- //******************---------MODAL POPUP CLOSE FUNCTION-------**************************// -->
