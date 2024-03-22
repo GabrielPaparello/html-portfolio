0ccdbe36d58343387d698719e41833c5bb0be875
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-03-22 12:03:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 
 //******************---------MODAL POPUP FUNCTION-------**************************// -->
-  const popup = document.querySelector('.popup');
+  const popup = $('.popup');
   window.onload = function (){
     setTimeout(function(){
       popup.classList.add("open-popup")
